906e3c67074f0ba965265302f0a6a91025006eb0
Mateus <mateusmacedoeng@gmail.com>
2024-02-10 10:00:51 -0300

    add actions

---
+++ source/focus-timer-v2/actions.js
@@ -0,0 +1,14 @@
+export function toggleRunning() {
+    console.log('toggleRunning function');
+}
+
+export function toggleReset() {
+    console.log('toggleReset function');
+}
+export function SetVolumeUp() {
+    console.log('volume up function');
+}
+
+export function SetVolumeDown() {
+    console.log('volume down function');
+}
--- source/focus-timer-v2/events.js
+++ source/focus-timer-v2/events.js
@@ -1,11 +1,12 @@
 import { controls } from './elements.js';
+import * as actions from './actions.js';
 export function registerControls() {
     controls.addEventListener('click', (event) => {
         const action = event.target.dataset.action;
-        if (action === undefined) {
+        if (typeof actions[action] != 'function') {
             return;
-        } else {
-            console.log(action);
         }
+
+        actions[action]();
     });
 }
