3d570cbe9db6fbaf008f858d59dffe5dce5d7cd1
Mateus <mateusmacedoeng@gmail.com>
2024-02-10 08:46:57 -0300

    fix light and dark mode toggle

--- source/toggle-mode.js
+++ source/toggle-mode.js
@@ -1,1 +1,13 @@
 let lightMode = true;
+const buttonToggle = document.getElementById('toggle-mode');
+
+buttonToggle.addEventListener('click', (event) => {
+    document.documentElement.classList.toggle('darkmode');
+
+    const mode = lightMode ? 'dark' : 'light';
+    event.currentTarget.querySelector(
+        'span',
+    ).textContent = `${mode} mode ativado`;
+
+    lightMode = !lightMode;
+});
