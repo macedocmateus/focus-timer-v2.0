37042d2b755b0a9c83f4001489029e2260ab4fec
Mateus <mateusmacedoeng@gmail.com>
2024-02-12 10:23:23 -0300

    add new audio

--- source/focus-timer-v2/actions.js
+++ source/focus-timer-v2/actions.js
@@ -1,10 +1,13 @@
 import * as element from './elements.js';
 import state from './state.js';
 import * as timer from './timer.js';
+import * as sounds from './sounds.js';
 
 export function toggleRunning() {
     state.isRunning = document.documentElement.classList.toggle('running');
     timer.countdown();
+
+    sounds.buttonAudio.play();
 }
 
 export function toggleReset() {
--- source/focus-timer-v2/sounds.js
+++ source/focus-timer-v2/sounds.js
@@ -1,3 +1,4 @@
+export const buttonAudio = new Audio('./assets/button-press.wav');
 export const cafeAudio = new Audio('./assets/cafeteria.wav');
 export const rainAudio = new Audio('./assets/chuva.wav');
 export const florestAudio = new Audio('./assets/floresta.wav');
