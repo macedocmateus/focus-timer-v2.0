f4bc04e27ff4ee1adc4a7733f672deb98d9b02af
Mateus <mateusmacedoeng@gmail.com>
2024-02-12 10:14:33 -0300

    add sounds

---
+++ source/focus-timer-v2/sound.js
@@ -0,0 +1,4 @@
+export const cafeAudio = new Audio('./assets/cafeteria.wav');
+export const rainAudio = new Audio('./assets/chuva.wav');
+export const florestAudio = new Audio('./assets/floresta.wav');
+export const bonfireAudio = new Audio('./assets/lareira.wav');
