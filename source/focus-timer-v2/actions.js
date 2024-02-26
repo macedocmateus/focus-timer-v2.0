848a6fc5326ff2359a6e5543686a8a8fb11e8eab
Mateus <mateusmacedoeng@gmail.com>
2024-02-26 07:34:11 -0300

    initial audio

--- source/focus-timer-v2/actions.js
+++ source/focus-timer-v2/actions.js
@@ -1,8 +1,10 @@
 import state from './state.js';
 import * as timer from './timer.js';
+import * as sounds from './sounds.js';
 export function toggleRunning() {
     state.isRunning = document.documentElement.classList.toggle('running');
     timer.countdown();
+    sounds.buttonPressAudio.play();
 }
 
 export function stop() {
@@ -13,6 +15,7 @@ export function stop() {
 export function setIncrement() {}
 
 export function setDecrement() {}
+
 export function tree() {}
 export function rain() {}
 export function storefront() {}
---
+++ source/focus-timer-v2/sounds.js
@@ -0,0 +1,6 @@
+export const buttonPressAudio = new Audio('./assets/button-press.wav');
+
+export const florest = new Audio('./assets/floresta.wav');
+export const rain = new Audio('./assets/chuva.wav');
+export const storefront = new Audio('./assets/cafeteria.wav');
+export const flame = new Audio('./assets/lareira.wav');
