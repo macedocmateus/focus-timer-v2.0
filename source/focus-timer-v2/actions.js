68a003a47a0ce9e0a38e71c63b69feff2dea1c10
Mateus <mateusmacedoeng@gmail.com>
2024-03-08 17:52:35 -0300

    card sound  interation

--- source/focus-timer-v2/actions.js
+++ source/focus-timer-v2/actions.js
@@ -1,13 +1,7 @@
 import state from './state.js';
 import * as timer from './timer.js';
 import * as sounds from './sounds.js';
-import {
-    cards,
-    treeIcon,
-    rainIcon,
-    storeFrontIcon,
-    flameIcon,
-} from './elements.js';
+import { treeIcon, rainIcon, storeFrontIcon, flameIcon } from './elements.js';
 export function toggleRunning() {
     state.isRunning = document.documentElement.classList.toggle('running');
     timer.countdown();
@@ -28,54 +22,64 @@ export function setDecrement() {
 }
 
 export function tree() {
-    treeIcon.classList.toggle('cor2');
-
-    if (isActive == false) {
+    if (state.isMute == false) {
+        state.isMute = document.documentElement.classList.remove('music-on');
+        treeIcon.classList.remove('cor2');
         sounds.florest.pause();
         return;
     } else {
+        state.isMute = document.documentElement.classList.add('music-on');
+        treeIcon.classList.add('cor2');
+
         sounds.florest.play();
         sounds.rain.pause();
         sounds.storefront.pause();
         sounds.flame.pause();
+
+        rainIcon.classList.remove('cor2');
     }
 }
 export function rain() {
-    rainIcon.classList.toggle('cor2');
-
-    if (isActive == false) {
+    if (state.isMute == false) {
+        state.isMute = document.documentElement.classList.remove('music-on');
+        rainIcon.classList.remove('cor2');
         sounds.rain.pause();
         return;
     } else {
+        state.isMute = document.documentElement.classList.add('music-on');
+        rainIcon.classList.add('cor2');
+
         sounds.florest.pause();
         sounds.rain.play();
         sounds.storefront.pause();
         sounds.flame.pause();
-    }
-}
-export function storefront() {
-    storeFrontIcon.classList.toggle('cor2');
 
-    if (isActive == false) {
-        sounds.storefront.pause();
-        return;
-    } else {
-        sounds.florest.pause();
-        sounds.rain.pause();
-        sounds.storefront.play();
-        sounds.flame.pause();
+        treeIcon.classList.remove('cor2');
     }
 }
-export function flame() {
-    flameIcon.classList.toggle('cor2');
-
-    if (isActive == false) {
-        sounds.flame.pause();
-        return;
-    } else {
-        sounds.florest.pause();
-        sounds.rain.pause();
-        sounds.storefront.pause();
-        sounds.flame.play();
-    }
-}
+// export function storefront() {
+//     if (state.isMute == false) {
+//         storeFrontIcon.classList.remove('cor2');
+//         sounds.storefront.pause();
+//         return;
+//     } else {
+//         storeFrontIcon.classList.add('cor2');
+//         sounds.florest.pause();
+//         sounds.rain.pause();
+//         sounds.storefront.play();
+//         sounds.flame.pause();
+//     }
+// }
+// export function flame() {
+//     if (state.isMute == false) {
+//         flameIcon.classList.remove('cor2');
+//         sounds.flame.pause();
+//         return;
+//     } else {
+//         flameIcon.classList.add('cor2');
+//         sounds.florest.pause();
+//         sounds.rain.pause();
+//         sounds.storefront.pause();
+//         sounds.flame.play();
+//     }
+// }
