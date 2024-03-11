9f3746c56b3001dde31d527eb6866a43dfbbbec7
Mateus <mateusmacedoeng@gmail.com>
2024-03-11 14:56:56 -0300

    test sound

--- source/focus-timer-v2/sounds.js
+++ source/focus-timer-v2/sounds.js
@@ -1,6 +1,6 @@
 export const buttonPressAudio = new Audio('./assets/button-press.wav');
 export const kitchenTimer = new Audio('./assets/kitchen-timer.mp3');
-export const florest = new Audio('./assets/Floresta.wav');
+export const florest = new Audio('./assets/tree.wav');
 export const rain = new Audio('./assets/Chuva.wav');
 export const storefront = new Audio('./assets/Cafeteria.wav');
 export const flame = new Audio('./assets/Lareira.wav');
