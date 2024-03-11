5ceac0f9c83e6ce5dcaa5055f4ab2511d2206449
Mateus <mateusmacedoeng@gmail.com>
2024-03-11 14:35:23 -0300

    bug fix sounds

--- source/focus-timer-v2/sounds.js
+++ source/focus-timer-v2/sounds.js
@@ -1,9 +1,9 @@
-export const buttonPressAudio = new Audio('../../assets/button-press.wav');
-export const kitchenTimer = new Audio('../../assets/kitchen-timer.mp3');
-export const florest = new Audio('../../assets/floresta.wav');
-export const rain = new Audio('../../assets/chuva.wav');
-export const storefront = new Audio('../../assets/cafeteria.wav');
-export const flame = new Audio('../../assets/lareira.wav');
+export const buttonPressAudio = new Audio('./assets/button-press.wav');
+export const kitchenTimer = new Audio('./assets/kitchen-timer.mp3');
+export const florest = new Audio('./assets/floresta.wav');
+export const rain = new Audio('./assets/chuva.wav');
+export const storefront = new Audio('./assets/cafeteria.wav');
+export const flame = new Audio('./assets/lareira.wav');
 
 florest.loop = true;
 rain.loop = true;
